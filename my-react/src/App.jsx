a9b8309228003ab028511f204b86054ff64764b8
sawan44 <sawanpanwarx@gmail.com>
2025-06-22 11:57:39 +0530

    modified-code

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,26 +1,19 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import Details from './Details'
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Contact from './Pages/Contact'
+import Header from './Header'
 
 const App = () => {
-
-  const [data, setdata] = useState([])
-
-  const getdata = async ()=>{
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
-    setdata(response.data)
-  }
-
   return (
     <div>
-      <button onClick={getdata} className='m-5 bg-blue-400 px-6 py-3 rounded text-xl text-white font-semibold active:scale-90 cursor-pointer'>Get Data</button>
-      <div className='m-5 p-5 bg-gray-700'>
-        {data.map((elem)=>{
-          return <div>
-            < Details elem={elem}/>
-          </div>
-        })}
-      </div>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Contact' element={<Contact />} />
+      </Routes>
     </div>
   )
 }
